22ac8b87b6689d069da24c53e18a05bf0b17c416
johnwilsonks <jowilsonks@gmail.com>
2019-09-05 19:35:36 +0100

    DB Initialization

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const config = require("config");
+const db = config.get("mongoURI");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true
+    });
+    console.log("MonogoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require("./config/db");
 
+//Connect to MongoDB
+connectDB();
+
+//Root Route
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome to gifoetry API" });
 });
 
+//Listen to the port
 app.listen(PORT, () => console.log("Server started at the port " + PORT));
