14e2d91fb27a3ce2a6c1f8f27af4d914f0d0715d
johnwilsonks <jowilsonks@gmail.com>
2019-09-10 11:12:54 +0100

    Token expiry set to 1 hour

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -72,7 +72,7 @@ router.post(
       jwt.sign(
         payload,
         config.get("secret"),
-        { expiresIn: 360000 },
+        { expiresIn: 3600 },
         (err, token) => {
           if (err) throw err;
           res.json({ token });
